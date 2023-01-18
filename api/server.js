cc15bcdbe4ca8df084e6b8be3a66670918e89e8e
canasmh <canasmh@yahoo.com>
2023-01-18 13:26:53 -0500

    Create server and listen in on port 3000

--- api/server.js
+++ api/server.js
@@ -1,1 +1,12 @@
 // build your server here and require it from index.js
+
+const express = require('express');
+const server = express();
+
+server.get('/', (req, res) => {
+    res.send("Hello, World!");
+})
+
+
+
+module.exports = server;
