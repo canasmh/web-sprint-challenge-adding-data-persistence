018a4ab64e99ea3fff662d603a0c69d95cb71a43
canasmh <canasmh@yahoo.com>
2023-01-18 14:13:30 -0500

    Set up the resources api and model

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,25 @@
 // build your `Resource` model here
+// build your `Project` model here
+const db = require('../../data/dbConfig');
+
+const find = async () => {
+    const resources = await db('resources');
+
+    return resources;
+}
+
+const add = async (resource) => {
+    try {
+        const id = await db('resources').insert(resource);
+        const newResource = await db('resources').where({resource_id: id}).first();
+        return newResource;
+    } catch (err) {
+        return {status: 400, message: "There was an error inserting new resource"}
+    }
+    
+}
+
+module.exports = {
+    find,
+    add
+}
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,22 @@
 // build your `/api/resources` router here
+const express = require('express');
+const router = express.Router();
+const resourceModel = require('./model');
+
+router.get('/', async (req, res) => {
+    const resources = await resourceModel.find();
+
+    res.send(resources).json();
+});
+
+router.post('/', async (req, res) => {
+    const resource = await resourceModel.add(req.body);
+    if (resource.status) {
+        res.sendStatus(resource.status)
+    } else {
+        res.send(resource).json();
+    }
+    
+});
+
+module.exports = router;
