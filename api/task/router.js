d9fdea10ab9ad27117d9292ff91cfa7b89a00b1f
canasmh <canasmh@yahoo.com>
2023-01-18 14:50:14 -0500

    Set up task api and model

--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,22 @@
 // build your `/api/tasks` router here
+const express = require('express');
+const router = express.Router();
+const taskModel = require('./model');
+
+router.get('/', async (req, res) => {
+    const task = await taskModel.find();
+
+    res.send(task).json();
+});
+
+router.post('/', async (req, res) => {
+    const task = await taskModel.add(req.body);
+    if (task.status) {
+        res.sendStatus(task.status)
+    } else {
+        res.send(task).json();
+    }
+    
+});
+
+module.exports = router;
